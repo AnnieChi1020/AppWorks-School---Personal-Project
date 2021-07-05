70c5e55655bc3856d90bbf6962fe3b09503f1335
Annie Chi <annie.chi1020@gmail.com>
2021-07-05 09:43:28 +0800

    remove unused useSelector

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import {
   getEvents,
   updateEvent,
